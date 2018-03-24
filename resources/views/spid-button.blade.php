c9dd170e13685ac24f47a495b7f93eecf36052e2
Davide Porrovecchio <pdavide@gmail.com>
2018-03-24 15:30:13 +0100

    Updated spid-smart-button assets

--- resources/views/spid-button.blade.php
+++ resources/views/spid-button.blade.php
@@ -1,7 +1,5 @@
 <div class="agid-spid-enter-button" aria-live="polite" data-size="{{ $size or 'm' }}"></div>
 @push('scripts')
-    <!-- Promise polyfill -->
-    <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
     <script type="text/javascript" src="{{ asset('/vendor/spid-auth/js/agid-spid-enter.min.latest.js') }}"></script>
     <script type="text/javascript">
       window.agidSpidEnter.init({
