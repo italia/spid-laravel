07e9844ae992f34f6d161fba858cba8ac7c20f21
Davide Porrovecchio <pdavide@gmail.com>
2019-03-11 11:27:40 +0100

    Change 'or' to '??' null-coalesce operator

--- resources/views/spid-button.blade.php
+++ resources/views/spid-button.blade.php
@@ -1,4 +1,4 @@
-<div class="agid-spid-enter-button" aria-live="polite" data-size="{{ $size or 'm' }}"></div>
+<div class="agid-spid-enter-button" aria-live="polite" data-size="{{ $size ?? 'm' }}"></div>
 @push('scripts')
     <script type="text/javascript" src="{{ asset('/vendor/spid-auth/js/agid-spid-enter.min.latest.js') }}"></script>
     <script type="text/javascript">
