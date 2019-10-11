987211a85c84cee6156333c5aad1e2eff76b63af
Davide Porrovecchio <pdavide@gmail.com>
2019-10-11 12:42:56 +0200

    Add SPIDLoginAnomalyException

--- resources/lang/it/messages.php
+++ resources/lang/it/messages.php
@@ -2,7 +2,6 @@
 /**
  * This file contains the strings needed for SPIDAuth Package Italian translation.
  *
- * @package Italia\SPIDAuth
  * @license BSD-3-clause
  */
 
@@ -10,5 +9,14 @@ return [
     'spid_login' => "Effettua l'accesso con SPID",
     'spid_access_required' => "La risorsa richiesta richiede l'accesso con SPID.",
     'return_home' => 'ritorna alla pagina iniziale',
-    'or' => 'oppure'
+    'or' => 'oppure',
+
+    'anomalies' => [
+        19 => 'autenticazione fallita per ripetuto inserimento di credenziali errate',
+        20 => 'utente privo di credenziali compatibili con il livello SPID richiesto',
+        21 => 'tempo scaduto durante il processo di autenticazione',
+        22 => "negato il consenso all'invio dei dati richiesti per il servizio",
+        23 => 'utente con identità sospesa/revocata o con credenziali bloccate',
+        25 => "processo di autenticazione annullato dall'utente",
+    ],
 ];
