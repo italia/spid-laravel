229acab40c28c79003350058b6239bfac56fc493
tmoreal <t.moreal@twobeesolution.com>
2020-09-28 08:58:50 +0200

    :sparkles: Ora lista idp viene presa dal file di configurazione della stessa, ora se lista è molto alta appare una scrollbar, rifatto in js vanilla script per idp selezionato

--- resources/views/login-spid.blade.php
+++ resources/views/login-spid.blade.php
@@ -1,6 +1,6 @@
 @inject('SPIDAuth', 'SPIDAuth')
 
-<!DOCTYPE html>
+        <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
@@ -17,32 +17,33 @@
     @stack('styles')
 </head>
 <body>
-    <div id="app" class="flex-center position-ref full-height">
-        <main class="py-4">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-8">
-                        <div class="card">
-                            <div class="card-header">
-                                @if (session('url.intended'))
+<div id="app" class="flex-center position-ref full-height">
+    <main class="py-4">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                            @if (session('url.intended'))
                                 @lang('spid-auth::messages.spid_access_required')
-                                @else
+                            @else
                                 @lang('spid-auth::messages.spid_login')
-                                @endif</div>
-                            <div class="card-body">
-                                @include('spid-auth::spid-button', ['size' => 'm'])
-                                @lang('spid-auth::messages.or') <a href="{{ url('/') }}">@lang('spid-auth::messages.return_home')</a>
-                            </div>
+                            @endif</div>
+                        <div class="card-body">
+                            @include('spid-auth::spid-button', ['size' => 'm'])
+                            @lang('spid-auth::messages.or') <a
+                                    href="{{ url('/') }}">@lang('spid-auth::messages.return_home')</a>
                         </div>
                     </div>
                 </div>
             </div>
-            @yield('content')
-        </main>
-    </div>
+        </div>
+        @yield('content')
+    </main>
+</div>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @stack('scripts')
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@stack('scripts')
 </body>
 </html>
